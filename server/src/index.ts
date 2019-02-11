8a8759609afec05ccc61142776f8cde705f9f4a2
Michael Helvey <michael.helvey1@gmail.com>
2019-02-10 16:17:11 -0800

    chore: updated dev build & dev cors url

--- server/src/index.ts
+++ server/src/index.ts
@@ -33,7 +33,7 @@ const server = new ApolloServer({
 
 const app = express()
 
-const whitelist = ['http://localhost:3000']
+const whitelist = ['http://localhost:3000', 'http://localhost:5000']
 const corsOptions = {
   origin: function(origin: any, callback: any) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
